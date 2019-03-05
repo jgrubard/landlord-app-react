4b6f9aa104b9bd09e4de8a75c87c850edd86d0d5
Jeremy Grubard <jgrubard@gmail.com>
2019-03-05 16:10:45 -0500

    change url to deployed app

--- src/store/production_url.js
+++ src/store/production_url.js
@@ -1,6 +1,8 @@
 let production_url;
-const local_api_url = 'http://192.168.1.175:3000/v1/tenant_applications/';
+// const local_api_url = 'http://192.168.1.175:3000';
+const deployed_api_url = 'https://landlord-app-ruby-api.herokuapp.com';
 
-production_url = local_api_url;
+// production_url = local_api_url + '/v1/tenant_applications/';
+production_url = deployed_api_url + '/v1/tenant_applications/';
 
 export default production_url;
